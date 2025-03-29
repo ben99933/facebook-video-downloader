4f859ed3beda686f8dac5770016089405c224e3c
ben99933 <yangben999@gmail.com>
2025-03-29 15:37:45 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/facebook-video-downloader/',
   plugins: [
     vue(),
     vueJsx(),
